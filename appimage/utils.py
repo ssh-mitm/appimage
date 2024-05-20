c6e2b31f5d04f640a04753159ffbb7023b96c509
Manfred Kaiser <manfred.kaiser@ssh-mitm.at>
2024-05-20 17:48:23 +0200

    fixed some bugs

--- appimage/utils.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-__all__ = ["metadata", "resources"]
-
-if sys.version_info >= (3, 10):
-    from importlib import metadata, resources
-else:
-    import importlib_metadata as metadata
-    import importlib_resources as resources
